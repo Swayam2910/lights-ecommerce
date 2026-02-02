1c1dae865d144bfcc15780d820898c0ef9ec1a7c
Swayam Dheer <swayamdheer@Swayams-MacBook-Pro.local>
2026-02-02 17:45:14 +0530

    Basic model setup serializer views and migrations

--- backend/omlights/models.py
+++ backend/omlights/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        max_digits = 10,
+        decimal_places=2
+    )
+    is_active = models.BooleanField(default=True)
+    is_priority = models.BooleanField(default=False)
+    category=models.CharField(
+        max_length=100
+    )
+    added_at=models.DateTimeField(auto_now_add=True)
+
--- backend/omlights/views.py
+++ backend/omlights/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.filter(is_active=True).order_by('-is_priority', '-added_at')
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'category', 'description']
+    ordering_fields = ['price', 'added_at', 'is_priority']
+    ordering = ['-is_priority', '-added_at']
