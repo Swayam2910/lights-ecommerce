d102e102e9e2d0c094906de915d939707d0d878e
Swayam Dheer <swayamdheer@Swayams-MacBook-Pro.local>
2026-02-17 16:53:51 +0530

    Docker setup

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -85,12 +85,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
